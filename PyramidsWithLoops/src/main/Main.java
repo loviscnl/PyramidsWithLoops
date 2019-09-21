548c03dd4911fd81d6a2de1a96dd4f17969f1e89
nicholasloviscek <nicholasloviscek@192.168.1.13>
2019-09-20 20:44:28 -0400

    new changes 9/20/2019

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,7 +1,9 @@
 /*
  * Nicholas Loviscek
  * loviscnl@mail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
+ * Printing pyramids using loops. It kind-of works.
+ * Due: 9/26/2019
+ * IS4010: Java Programming 
  * What I really want is
         1
        222
@@ -15,15 +17,22 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 1; i <= lines; i++) {
-			for (int j = 1; j < i+1; j++) {
-				System.out.print(j);
-			}
-			System.out.println(" ");
-			
-			
-		}
+public static void main(String[] args) {
+	int odd = 1;
+	int lines = 5;
+	int spaces = 4;
+	for (int i = 1; i <= lines; i++) 
+	{
+		for (int k=1; k<=spaces; k++)
+		{System.out.print(" ");}
+		
+		for (int j = 1; j <= odd; j++)
+		{System.out.print(i);}
+		
+			System.out.println();
+			odd = odd+2;
+			spaces=spaces-1;
 	}
+  }
 }
+
