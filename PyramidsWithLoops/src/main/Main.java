bc57928c6cc485514ab79bf85f166a5849787d66
loviscnl <loviscnl@LCBLAB2230-31.uccob.uc.edu>
2019-09-25 11:23:21 -0400

    Final changes

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -4,32 +4,31 @@
  * Printing pyramids using loops. It kind-of works.
  * Due: 9/26/2019
  * IS4010: Java Programming 
- * What I really want is
+ * Description: What I really want is
         1
        222
       33333
      4444444
     555555555 
     
-    team then commit
 */
 package main;
 
 public class Main {
 
 public static void main(String[] args) {
-	int odd = 1;
-	int lines = 5;
-	int spaces = 4;
-	for (int i = 1; i <= lines; i++) 
-	{
-		for (int k=1; k<=spaces; k++)
+	int odd = 1;  //declared variable for numbers per row
+	int lines = 5;  //declared variable for number of lines
+	int spaces = 4;  //declared variable for number of spaces
+	for (int i = 1; i <= lines; i++)  // loop to get correct number of lines for the pyramid
+	{ 
+		for (int k=1; k<=spaces; k++)  // loop to get correct number of spaces to make the pyramid
 		{System.out.print(" ");}
 		
-		for (int j = 1; j <= odd; j++)
+		for (int j = 1; j <= odd; j++)   // loop to have the correct number of numbers per row
 		{System.out.print(i);}
 		
-			System.out.println();
+			System.out.println();   // creates the 1,3,5,7,9 order of numbers per row
 			odd = odd+2;
 			spaces=spaces-1;
 	}
